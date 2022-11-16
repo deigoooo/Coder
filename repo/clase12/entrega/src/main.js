193d27c32657aad8034f2b1bae66553895014da7
deigoooo <df.ramirezz@gmail.com>
2022-11-16 17:41:17 -0300

    movi el entregable a la clase correspondiente

--- repo/clase12/entrega/src/main.js
+++ repo/clase12/entrega/src/main.js
@@ -1,32 +1,61 @@
+//Servidor************
 const express = require('express');
-const { Server: HttpServer } = require('http');
-const { Server: IOServer } = require('socket.io');
+const moment = require('moment');
+const aplicacion = express();
+const { Server: HttpServer } = require("http");
+const { Server: IOServer } = require("socket.io");
+const Contenedor = require("./contenedor/contenedorFs");
 
-//configuracion
-const app = express();
-const httpServer = new HttpServer(app);
+const port = 8080;
+const publicRoot = "./public";
+
+const httpServer = new HttpServer(aplicacion);
 const io = new IOServer(httpServer);
 
-//Variables
-const PORT = 8080;
-const publicRoot = './public';
-const messages = [
-    { author: "Juan", text: "Hola! que tal?"},
-    { author: "Pedro", text: "Muy bien! vos?"},
-    { author: "Ana", text: "Genial!!!"},
-];
+//Lineas para usar json
+aplicacion.use(express.json());
+aplicacion.use(express.urlencoded({ extended: true }));
 
-//Carpeta publica visible
-app.use(express.static(publicRoot));
+//***** Hacemos la carpeta public visible
+aplicacion.use(express.static(publicRoot));
+//****************
 
-//ServerUP
-httpServer.listen (PORT, function(){
-    console.log('Servidor corriendo en http://localhost:8080');
-})
+const productos = new Contenedor("./src/db/productos.txt");
+const messages = new Contenedor("./src/db/mensajes.txt");
 
-//Configuracion de sockets
-io.on('connection', function(socket){
-    console.log('Un cliente se ha conectado');
-    socket.emit('messages', messages);
+//Endpoints***
+aplicacion.get("/", (peticion, respuesta) => {
+  respuesta.send("index.html", { root: publicRoot });
 });
 
+//Servidor************
+const servidor = httpServer.listen(port, () => {
+  console.log(`Servidor escuchando: ${servidor.address().port}`);
+});
+
+servidor.on("error", (error) => console.log(`Error: ${error}`));
+//****************
+
+//sockets
+io.on("connection", async (socket) => {
+  console.log("Nuevo cliente conectado");
+
+  const listaProductos = await productos.getAll();
+  socket.emit("nueva-conexion", listaProductos);
+
+  socket.on("new-product", (data) => {
+    productos.save(data);
+    io.sockets.emit("producto", data);
+  });
+  //Para enviar todos los mensajes en la primera conexion
+  const listaMensajes = await messages.getAll();
+  socket.emit("messages", listaMensajes);
+
+  //Evento para recibir nuevos mensajes
+  socket.on("new-message", async (data) => {
+    data.time= moment(new Date()).format('DD/MM/YYYY hh:mm:ss');
+    await messages.save(data);
+    const listaMensajes = await messages.getAll();
+    io.sockets.emit("messages", listaMensajes);
+  });
+});
