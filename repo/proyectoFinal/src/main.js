4a07dd7efaf82cb06b6beac6be259a2fb6d68e16
deigoooo <df.ramirezz@gmail.com>
2022-11-21 16:08:56 -0300

    aplicando middleware

--- repo/proyectoFinal/src/main.js
+++ repo/proyectoFinal/src/main.js
@@ -1,16 +1,33 @@
 const express = require('express');
-const moment = require('moment');
 const { Server: HttpServer } = require('http');
 const { Server: IOServer } = require('socket.io');
+
+//importamos las rutas
+import { rutaProducto } from './routes/productos.js';
+import { rutaCarrito } from './routes/carrito.js';
+
 const app = express();
 
-//declaracion de variables
-const port= 8080;
+//implementacion port & carpeta publica
+const port = process.env.PORT || 8080;
 const publicRoot = './public';
+
+//implementamos las rutas
+app.use('/api/producto', rutaProducto);
+app.use('/api/carrito', rutaCarrito);
 
 //uso de json
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//middleware
+app.use((peticion, respuesta, next) => {
+    const key = peticion.headers.key;
+    if(key == 1234){
+      next();//Autorizar ejecutar lo siguiente
+    }
+    respuesta.status(403).send('Acceso denegado!');
+  });
 
 //inicializacion sockets
 const httpServer = new HttpServer(app);
@@ -19,16 +36,11 @@ const io = new IOServer(httpServer);
 //carpeta publica visible
 aplicacion.use(express.static(publicRoot));
 
-//endpoints
-app.get('/', (peticion, respuesta) => {
-    
-  });
-
 //inicializo el server
-const servidor = httpServer.listen(port, () => {
+const servidor = app.listen(port, () => {
     console.log(`Servidor escuchando: ${servidor.address().port}`);
   });
   
   servidor.on('error', error => console.log(`Error: ${error}`));
 
-//sockets
+
