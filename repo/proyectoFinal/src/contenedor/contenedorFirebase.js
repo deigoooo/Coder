6df922b4b8be3354a4d77d3c2d49b0d1449bc95e
deigoooo <df.ramirezz@gmail.com>
2022-12-14 15:54:17 -0300

    aplicando firebase

--- repo/proyectoFinal/src/contenedor/contenedorFirebase.js
+++ repo/proyectoFinal/src/contenedor/contenedorFirebase.js
@@ -1,12 +1,64 @@
-const admin = require("firebase-admin");
-
-const serviceAccount = require("../proyectofinal.json");
+import admin from 'firebase-admin';
+import serviceAccount from '../config.js';
+//const serviceAccount = require("../proyectofinal.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount.firebase)
 });
 
 
-(async () => {
-    
-})();
+export class Contenedor {
+  constructor(db, query ) {
+    this.db= admin.firestore();
+    this.query = db.collections('ecommerce')
+  }
+
+  async save(objeto) {
+
+    let doc = await this.query.add(objeto);
+
+    return doc.id;
+
+  }
+
+  async getById(id) {
+
+    const doc = await this.query.get();
+    doc.array.forEach(element => {
+      if (element.id == id) {
+        return element;
+      }
+      return null;
+    });
+
+  }
+
+  async update(producto) {
+
+    await this.coleccion.updateOne({'_id': producto._id}, producto);
+
+  }
+
+  async getAll() {
+
+    let docs = await this.query.get();
+    docs.array.forEach(element => {
+      const doc = {id: element.id, ...element.data()};
+      return doc;
+    });
+  
+  }
+
+  async deleteById(id) {
+
+    const doc = await this.coleccion.deleteOne({ '_id': id });
+
+  }
+
+  async deleteAll() {
+
+    const doc = await this.coleccion.delete();
+  
+  }
+
+}
