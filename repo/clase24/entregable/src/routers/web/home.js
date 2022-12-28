3e6593b783bcf67e2a5dbeb747d17204193447db
deigoooo <df.ramirezz@gmail.com>
2022-12-28 20:14:40 -0300

    ultima modificacion

--- repo/clase24/entregable/src/routers/web/home.js
+++ repo/clase24/entregable/src/routers/web/home.js
@@ -6,7 +6,12 @@ const productosWebRouter = new Router()
 productosWebRouter.get('/home', (req, res) => {
     //Cambiar el nombre de usuario
     const nombre = req.session.nombre
-    res.render(process.cwd() + '/views/pages/home.ejs', { nombre: `${nombre}` });
+    if (!nombre){
+        res.redirect('/login');
+    }else{
+        res.render(process.cwd() + '/views/pages/home.ejs', { nombre: `${nombre}` });
+    }
+    
 })
 
 productosWebRouter.get('/productos-vista-test', (req, res) => {
